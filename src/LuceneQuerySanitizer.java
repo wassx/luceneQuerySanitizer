801b2861b785748cca9f2b3b78bed9b091a1821b
wassx <wassx@gmx.net>
2016-10-01 22:28:26 +0200

    Initial commit

---
+++ src/LuceneQuerySanitizer.java
@@ -0,0 +1,20 @@
+import java.util.regex.Pattern;
+import org.apache.commons.lang.StringEscapeUtils;
+
+/**
+ * Created by stefanwasserbauer (https://github.com/wassx) on 01.10.16.
+ * Based on http://lucene.apache.org/core/2_9_4/queryparsersyntax.html#Escaping%20Special%20Characters
+ */
+public class LuceneQuerySanitizer {
+
+	private final static Pattern SPECIAL_REGEX_CHARS = Pattern.compile("[{}()\\[\\]+&|-~*?!:^$]");
+
+	public static String sanitize(String query) {
+		String escaped = StringEscapeUtils.escapeJavaScript(query);
+		escaped = SPECIAL_REGEX_CHARS.matcher(escaped).replaceAll("\\\\$0");
+		escaped = escaped.replaceAll("AND", "\\A\\N\\D");
+		escaped = escaped.replaceAll("OR", "\\O\\R");
+		escaped = escaped.replaceAll("NOT", "\\N\\O\\T");
+		return escaped;
+	}
+}
