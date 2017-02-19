d1177907c76b5b5b3fad192fa4713d8073285839
wassx <wassx@gmx.net>
2017-02-19 21:12:13 +0100

    Fix escaping

--- src/LuceneQuerySanitizer.java
+++ src/LuceneQuerySanitizer.java
@@ -13,8 +13,7 @@ public class LuceneQuerySanitizer {
 	private final static Pattern SPECIAL_REGEX_CHARS = Pattern.compile("[\\{\\}\\(\\)\\[\\]\\+\\&\\|\\-\\~\\*\\?\\!\\:\\^\\$]");
 
 	public static String sanitize(String query) {
-		String escaped = StringEscapeUtils.escapeJavaScript(query);
-		escaped = SPECIAL_REGEX_CHARS.matcher(escaped).replaceAll("\\\\$0");
+		String escaped = SPECIAL_REGEX_CHARS.matcher(query).replaceAll("\\\\$0");
 		escaped = escaped.replaceAll("AND", "\\A\\N\\D");
 		escaped = escaped.replaceAll("OR", "\\O\\R");
 		escaped = escaped.replaceAll("NOT", "\\N\\O\\T");
